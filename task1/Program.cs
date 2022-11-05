b2678dde87ae2e57e6b95c8acd78761f039eb2df
amberbay1 <117526226+amberbay1@users.noreply.github.com>
2022-11-05 18:04:43 +0300

    code
    add code

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] usersArray;
+Console.WriteLine("Введите массив строк через пробел:");
+string input = Console.ReadLine();
+usersArray = input.Split(' ');
+var arrayLength = new string[usersArray.Length];
+var counter = 0;
+string[] outputArray = new string[counter];
+foreach (var value in usersArray)
+{
+    if (value.Length <= 3)
+    {
+        arrayLength[counter] = value;
+        counter++;
+        Console.Write(value + " ");
+    }
+}
