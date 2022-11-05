27a9bf7dd3df1a75fe233a6aeb7705883e1c83e2
amberbay1 <117526226+amberbay1@users.noreply.github.com>
2022-11-05 19:07:20 +0300

    code update
    deleted unused code

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,7 +4,6 @@ string input = Console.ReadLine();
 usersArray = input.Split(' ');
 var arrayLength = new string[usersArray.Length];
 var counter = 0;
-string[] outputArray = new string[counter];
 foreach (var value in usersArray)
 {
     if (value.Length <= 3)
